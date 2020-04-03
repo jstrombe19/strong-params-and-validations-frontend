d39d7b773b0a7002a9fede4dfba9fdf9129c8cb6
jstrombe19 <jaredstromberg@gmail.com>
2020-04-03 13:20:04 -0700

    Wrote functions to display user information in cards as well as error messages if/when a new user instance does not meet validation requirements.

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
     function getExistingUserData() {
         fetch('http://localhost:3000/users')
         .then(response => response.json())
-        .then(result => console.log('users', result))
+        .then(users => users.forEach(user => {
+            createUserCard(user)
+        }))
     }
 
     getExistingUserData()
 
     const newUserForm = document.getElementById('add-new-user')
+    const userCardContainer = document.getElementById('user-card-container')
 
     newUserForm.addEventListener('submit', event => {
         event.preventDefault()
@@ -20,21 +23,58 @@ document.addEventListener('DOMContentLoaded', () => {
         const username = formData.get('username')
         const email = formData.get('email')
         const password = formData.get('password')
+        const newUser = { user: {
+            name: name,
+            username: username,
+            email: email,
+            password: password
+        }}
         fetch('http://localhost:3000/users', {
             method: "POST",
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({
-                user: {
-                    name: name,
-                    username: username,
-                    email: email,
-                    password: password
-                }
-            })
+            body: JSON.stringify(newUser)
         })
+        .then(response => response.json())
+        .then(result => handleUserResponse(result, newUserForm))
+        
     })
+
+    function createUserCard(user) {
+        const userCard = document.createElement('div')
+        userCard.className = "user-card"
+        const name = document.createElement('h3')
+        const username = document.createElement('p')
+        const email = document.createElement('p')
+        name.innerText = user.name
+        username.innerText = user.username
+        email.innerText = user.email
+        userCard.append(name, username, email)
+        userCardContainer.append(userCard)
+    }
+
+    function handleUserResponse(response, form) {
+        const errorMessage = document.getElementById('response-error-message')
+        if (errorMessage) {
+            newUserForm.removeChild(errorMessage)
+        }
+        if (response.user) {
+            createUserCard(response.user)
+            form.reset()
+        } else {
+            const responseKeys = Object.keys(response)
+            const responseMessage = document.createElement('ul')
+            responseMessage.id = "response-error-message"
+            responseMessage.innerText = "We were unable to create this user instance for the following reasons:"
+            responseKeys.forEach(responseKey => {
+                const li = document.createElement('li')
+                li.innerText = response[responseKey]
+                responseMessage.append(li)
+            })
+            newUserForm.append(responseMessage)
+        }
+    }
 
 })
